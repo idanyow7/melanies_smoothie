5443e308684359598e59ef64355659e12cccca10
idanyow7 <idanyow@gmail.com>
2025-01-12 12:41:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,9 @@ name_on_order = st.text_input("What is your name")
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+pdf_df=my_dataframe.to_pandas()
+st.dataframe(pdf_df)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
